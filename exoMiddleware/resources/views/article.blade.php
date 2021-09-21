4aaf1d26059d1e89b1fd260ef7104c6a8a512668
Seifyuuu <elgayas@hotmail.com>
2021-09-21 14:18:32 +0200

    part1

---
+++ exoMiddleware/resources/views/article.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.html')
+@section("content")
+    <h2 style="text-align: center">Articles</h2>
+@endsection
---
+++ exoMiddleware/resources/views/back/home.blade.php
@@ -0,0 +1,7 @@
+@extends("back.html")
+
+@section("content")
+
+<h2 style="text-align: center">Backoffice</h2>
+
+@endsection
